958c475bccc846a59bd41a25491de4039f4d893b
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2025-02-12 18:35:50 +0530

    Time: 4 ms (18.90%) | Memory: 97.2 MB (99.03%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-    void maxcalculate(vector<int>& prices,int i,int &maxprofit,int &minprice){
-        //base
-        if(i==prices.size()){
-            return;
-        }
-
-        //one case solve
-        if(prices[i]<minprice){
-            minprice=prices[i];
-        }
-        int todaysprofit=prices[i]-minprice;
-        if(todaysprofit>maxprofit){
-            maxprofit=todaysprofit;
-        }
-
-        //rest recursion will do
-        maxcalculate(prices,i+1,maxprofit,minprice);
-    }
     int maxProfit(vector<int>& prices) {
-        int maxprofit=INT_MIN;
-        int minprice=INT_MAX;
-        maxcalculate(prices,0,maxprofit,minprice);
-        return maxprofit;
+        int minb=INT_MAX;
+        int maxs=0;
+        for(int i=0;i<prices.size();i++){
+             minb=min(minb,prices[i]);
+             maxs=max(maxs,prices[i]-minb);
+        }
+        return maxs;
     }
 };
