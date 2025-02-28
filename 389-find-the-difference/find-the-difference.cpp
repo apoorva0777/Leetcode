ff8dfc7196a226531210b75515ba5d096e145ce2
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2025-02-28 19:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (27.20%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int hash[256]={0};
-        for(char ch:s){
-            hash[ch]++;
-        }
-        for(char ch:t){
-            hash[ch]--;
-            if(hash[ch]<0){
-                return ch;
-            }
-        }
-        return ' ';
+        char result=0;
+        for(char ch:s) result^=ch;
+        for(char ch:t) result^=ch;
+        return result;
     }
 };
