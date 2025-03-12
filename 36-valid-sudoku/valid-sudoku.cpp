4fab27a4fc734fe9234bfea2e6ebd1fd41b80fc9
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2025-03-12 10:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (70.25%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9]={0};
+        int col[9][9]={0};
+        int box[9][9]={0};
+
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board.size();j++){
+                if(board[i][j]=='.') continue;
+                int num=board[i][j]-'0'-1,k=i/3*3+j/3;
+                if(row[i][num]  || col[j][num] || box[k][num]) return false;
+                row[i][num]=col[j][num]=box[k][num]=1;
+            }
+            
+        }
+        return true;
+    }
+};
