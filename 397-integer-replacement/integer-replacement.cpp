a276ff7caf52047f52ddcb4233ee316e93136212
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2025-03-18 23:21:39 +0530

    Time: 2 ms (36.37%) | Memory: 7.7 MB (81.46%) - LeetSync

--- 397-integer-replacement/integer-replacement.cpp
+++ 397-integer-replacement/integer-replacement.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    int integerReplacement(int n) {
-        long num=n;
-        int cnt1=0;
-        while(num!=1){
-            if(num%2==0){
-                num=num/2;
-                cnt1++;
-            }else{
-                if(num==3 || (num-1)%4==0){
-                    num-=1;
-                    cnt1++;
-                }else{
-                    num+=1;
-                    cnt1++;
-                }
-            }
-        }
-        return cnt1;
+    int integerReplacement(long n) {  
+        if (n == 1) return 0;        
 
+        if (n % 2 == 0) {
+            return 1 + integerReplacement(n / 2); 
+        } else {
+            return 1 + min(integerReplacement(n - 1), integerReplacement(n + 1));  
+        }
     }
 };
