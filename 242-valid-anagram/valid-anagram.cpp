deacaf1eb789cd67fcf54de925e1e57b7de8c7e7
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2025-08-21 14:32:03 +0530

    Time: 4 ms (39.47%) | Memory: 9.7 MB (48.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()){
+        sort(t.begin(),t.end());
+        sort(s.begin(),s.end());
+        if(t==s){
+            return true;
+        }else{
             return false;
         }
-        int hash[256]={0};
-        for(char ch:s){
-            hash[ch]++;
-        }
-        for(char ch:t){
-            hash[ch]--;
-        }
-        for(int i=0;i<256;i++){
-            if(hash[i]!=0){
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 };
