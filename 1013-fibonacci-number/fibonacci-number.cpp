3427b554fa1b94b6c326d42372ea49deac9fcdbf
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2025-09-01 00:58:18 +0530

    Time: 3 ms (66.39%) | Memory: 8.4 MB (11.56%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        int prev2=0;
-        int prev=1;
-        if(n==1||n==0)return n;
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        if(n==0)return 0;
+        dp[1]=1;
         for(int i=2;i<=n;i++){
-            int curr=prev+prev2;
-            prev2=prev;
-            prev=curr;
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return prev;
+        return dp[n];
     }
 };
